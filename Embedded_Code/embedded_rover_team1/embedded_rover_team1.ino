37ca47ed669629dba6cb06308d7199168526017a
ConorCoggins <conorwcoggins@protonmail.com>
2025-10-16 17:15:58 -0500

    Added libraries to the embedded code. Might not be all that we need.

---
+++ Embedded_Code/embedded_rover_team1/embedded_rover_team1.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <SPI.h>
+#include "Adafruit_BMP3XX.h"
+
+void setup () {
+  Serial.begin(9600);
+}
+
+void loop () {
+  //SDA_Sensor
+  digitalWrite(6, )
+  
+  //SCL_Sensor
+  digitalWrite(7, )
+
+}
