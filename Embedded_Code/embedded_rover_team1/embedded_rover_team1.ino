5413474256fd865be0a45eb3b3fa3e8a2d4599d0
ConorCoggins <conorwcoggins@protonmail.com>
2025-10-17 15:16:40 -0500

    getting my shit together with embedded

--- Embedded_Code/embedded_rover_team1/embedded_rover_team1.ino
+++ Embedded_Code/embedded_rover_team1/embedded_rover_team1.ino
@@ -2,21 +2,28 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include "Adafruit_BMP3XX.h"
+#include <Adafruit_BMP3XX.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <SD.h>
 #include <Stepper.h>
+#include <Servo.h>
+
+Servo servo_25kg_turret_PWM_S1;
 
 void setup () {
   Serial.begin(9600);
+  Serial.print("Testing the Servo \n")
+
+  servo_25kg_turret_PWM_S1.attach()
+
 }
 
 void loop () {
   //SDA_Sensor
-  digitalWrite(6, )
-
+  
+  
   //SCL_Sensor
-  digitalWrite(7, )
+  
 
 }
