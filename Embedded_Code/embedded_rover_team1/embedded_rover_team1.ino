204df70ec369d2c2640b99208707f91a18c93bc4
ConorCoggins <conorwcoggins@protonmail.com>
2025-10-18 01:16:30 -0500

    stuff

--- Embedded_Code/embedded_rover_team1/embedded_rover_team1.ino
+++ Embedded_Code/embedded_rover_team1/embedded_rover_team1.ino
@@ -11,38 +11,40 @@
 
 Servo servo_25kg_turret_PWM_S1;
 Servo servo_55g_wrist_PWM_S2;
+Servo servo_45kg_claw_PWM_S3;
 
 void setup () {
   Serial.begin(9600);
   Serial.print("Testing the Servos \n");
 
   //Servo 1
-  // servo_25kg_turret_PWM_S1.attach(12);
+  servo_25kg_turret_PWM_S1.attach(12);
 
-  // Serial.print(" - Moving Servo 2 into Central Position \n");
-  // servo_25kg_turret_PWM_S1.writeMicroseconds(1500);
+  Serial.print(" - Moving Servo 1 into Central Position \n");
+  servo_25kg_turret_PWM_S1.writeMicroseconds(1500);
 
   //Servo 2 
   servo_55g_wrist_PWM_S2.attach(13);
 
   Serial.print(" - Moving Servo 2 into Central Position \n");
-  
+  servo_55g_wrist_PWM_S2.writeMicroseconds(1500);
+
+  //Servo 3
+  servo_45kg_claw_PWM_S3.attach(14);
+
+  Serial.print(" - Moving Servo 3 into Central Position \n");
+  servo_45kg_claw_PWM_S3.writeMicroseconds(1500);
 
 }
 
 void loop () {
-
+  
 
   servo_55g_wrist_PWM_S2.write(100);
   delay(2000);
   servo_55g_wrist_PWM_S2.write(0);
   delay(2000);
-  // servo_55g_wrist_PWM_S2.write(2);
-  // delay(200);
-  // servo_55g_wrist_PWM_S2.write(3);
-  // delay(200);
-  // servo_55g_wrist_PWM_S2.write(4);
-  // delay(200);
+  
   //SDA_Sensor
   
   
