5f331763fc0fa55b52008b5fa5087c5555d5d5a9
ConorCoggins <conorwcoggins@protonmail.com>
2025-10-21 16:41:24 -0500

    ros2

---
+++ ROS2/ros2_ws/src/my_package/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup q
+
+package_name = 'my_package'
+
+setup(
+    name=my_package,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='conorwcoggins@protonmail.com',
+    description='Test package while learning ROS',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest', 
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_node = my_package.my_node:main'
+        ],
+    },
+)
