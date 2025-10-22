c6db3b7df5607ef5581745c12caab95bec9fe1c7
aidanleibowitz <aml1273@uah.edu>
2025-10-21 20:27:00 -0500

    Update basestationlistener.py

--- ROS2/ros2_ws/src/my_package/my_package/basestationlistener.py
+++ ROS2/ros2_ws/src/my_package/my_package/basestationlistener.py
@@ -1,1 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float
+
+class basestationlistener(Node):
+  def __init__(self):
+    super().__init__('base_station_listener')
+    self.subscription = self.create_subscription(
+      Float,
+      'temperature',
+      self.listener_callback,
+      10)
+    self.subscription = self.create_subscription(
+      Float,
+      'pressure'
+      self.listener_callback,
+      10)
+    self.subscription = self.create_subscription(
+      Float,
+      'altitude',
+      self.listener_callback,
+      10)
+    self.subscription = self.create_subscription(
+      Float,
+      'acceleration',
+      self.listener_callback,
+      10)
+    self.get_logger().info('base station listener node started and listening for sensor data')
+  def listener_callback(self, msg):
+    self.get_logger().info(f'Recieved: "{msg.data}"')
+def main(args=None):
+  rclpy.init(args=args)
+  base_station_listener = basestationlistener()
+  basestationlistener().destroy_node()
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+  main()
 
