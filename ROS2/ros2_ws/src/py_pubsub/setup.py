71bb800aacc763aa11952b84a8116982088e8f44
aidanleibowitz <aml1273@uah.edu>
2025-10-22 21:41:49 -0500

    fixed typo

--- ROS2/ros2_ws/src/py_pubsub/setup.py
+++ ROS2/ros2_ws/src/py_pubsub/setup.py
@@ -22,7 +22,7 @@ setup(
             'pytest',
         ],
     },
-    eentry_points={
+    entry_points={
         'console_scripts': [
                 'talker = py_pubsub.publisher_member_function:main',
                 'listener = py_pubsub.subscriber_member_function:main',
