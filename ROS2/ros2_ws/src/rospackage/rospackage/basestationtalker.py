7133597f96cf0f4bb9e6de8930c26e8afab812e4
aidanleibowitz <aml1273@uah.edu>
2025-10-23 20:08:09 -0500

    updated so serial works in ros, will probably not work just a starting point

--- ROS2/ros2_ws/src/rospackage/rospackage/basestationtalker.py
+++ ROS2/ros2_ws/src/rospackage/rospackage/basestationtalker.py
@@ -1,4 +1,5 @@
 import rclpy
+import serial
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -7,6 +8,11 @@ class basestationtalker(Node):
     super().__init__('basestation_talker')
     self.publisher_ = self.create_publisher(String, 'command', 10)
 
+  def update_motor(self, wrist_position=0, claw_position=0, upper_elbow=0, lower_elbow=0, turret_position=0):
+    serial.write(f"W{wrist_position}C{claw_position}U{upper_elbow}L{lower_elbow}T{turret_position}\n".encode())
+    msg = String()
+    msg.data = f"W{wrist_position}C{claw_position}U{upper_elbow}L{lower_elbow}T{turret_position}"
+    self.publisher_.publish(msg)
 
 def main(args=None):
   rclpy.init(args=args)
